52682ccaa8b7bcbb731184b36b673bab99c0ab03
Bishal Mukherjee <bishalmukherjee448@gmail.com>
2023-06-09 23:11:51 +0530

    [FIX] server url modified

--- client/src/services/url.js
+++ client/src/services/url.js
@@ -1,1 +1,2 @@
-export const SERVER_URL = 'https://flash-sale-silk.vercel.app/api';
+export const SERVER_URL = 'https://flash-sale-five.vercel.app/api';
+// export const SERVER_URL = 'https://localhost:8080/api';
